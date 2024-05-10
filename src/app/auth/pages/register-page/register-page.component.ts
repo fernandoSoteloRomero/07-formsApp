ddb1b030ad9ba3dff24a283754f2fb938e3d9b9f
fernandoSoteloRomero <al169888@alumnos.uacj.mx>
2024-05-10 15:31:42 -0600

    WIP - validaciones asincronas

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider, emailPattern, firstNameAndLastnamePattern } from '../../../shared/validations/validators';
+import { ValidatorsService } from '../../../shared/service/validators.service';
+import { EmailValidatorService } from '../../../shared/validations/email-validators.service';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
 export class RegisterPageComponent {
+
+  constructor(
+    private fb:FormBuilder,
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidatorService
+  ){}
+
+
+  public myForm: FormGroup = this.fb.group({
+
+    name: ['', [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern)], [this.emailValidator]],
+    username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]]
+  })
+
+  isValidField(field: string){
+    return this.validatorsService.isValidField(this.myForm, field);
+  }
+
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
+
+
 
 }
